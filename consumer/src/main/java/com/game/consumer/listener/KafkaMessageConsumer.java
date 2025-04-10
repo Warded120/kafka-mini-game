c56fc072df6267eb8b2a44505e16b8e35abee52b
Warded120 <ihren@softserveinc.com>
2025-04-10 14:39:17 +0300

    implemented kafka consumer

--- consumer/src/main/java/com/game/consumer/listener/KafkaMessageConsumer.java
+++ consumer/src/main/java/com/game/consumer/listener/KafkaMessageConsumer.java
@@ -5,7 +5,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PersonKafkaConsumer {
+public class KafkaMessageConsumer {
     @KafkaListener(topics = "message", groupId = "kafka-mini-game")
     public void consume(Message message) {
         System.out.println(message.username()
